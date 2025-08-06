b4f312117ebcc53f2af9f80a99ac9f648d7154b8
strangka <strangka@student.21-school.ru>
2025-08-06 16:26:58 +0700

    all

--- QR-Code/src/tests/App.test.tsx
+++ QR-Code/src/tests/App.test.tsx
@@ -1,33 +1,33 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import { QRCode } from '../components/QRCode';
+import { describe, test, expect } from 'vitest'
+import { render, screen, fireEvent } from '@testing-library/react'
+import { QRCode } from '../components/QRCode'
 
 describe('QRCode Component', () => {
   test('render input field with URL placeholder and download buttons', () => {
-    render(<QRCode />);
+    render(<QRCode />)
     
-    expect(screen.getByPlaceholderText('URL')).toBeInTheDocument();
-    expect(screen.getByText('Download PNG')).toBeInTheDocument();
-    expect(screen.getByText('Download JPEG')).toBeInTheDocument();
-    expect(screen.getByText('Scan QR')).toBeInTheDocument();
-  });
+    expect(screen.getByPlaceholderText('URL')).toBeInTheDocument()
+    expect(screen.getByText('Download PNG')).toBeInTheDocument()
+    expect(screen.getByText('Download JPEG')).toBeInTheDocument()
+    expect(screen.getByText('Scan QR')).toBeInTheDocument()
+  })
 
   test('update input value', () => {
-    render(<QRCode />);
+    render(<QRCode />)
     
-    const input = screen.getByPlaceholderText('URL');
-    const testUrl = 'https://example.com';
+    const input = screen.getByPlaceholderText('URL')
+    const testUrl = 'https://example.com'
     
-    fireEvent.change(input, { target: { value: testUrl } });
+    fireEvent.change(input, { target: { value: testUrl } })
     
-    expect(input).toHaveValue(testUrl);
-  });
+    expect(input).toHaveValue(testUrl)
+  })
 
   test('should display QR', () => {
-    render(<QRCode />);
+    render(<QRCode />)
     
-    const input = screen.getByPlaceholderText('URL');
-    fireEvent.change(input, { target: { value: 'https://toemoe.github.io/qr-code-generator/' } });
-    expect(screen.getByRole('img')).toBeInTheDocument();
-  });
-});
+    const input = screen.getByPlaceholderText('URL')
+    fireEvent.change(input, { target: { value: 'https://example.com' } })
+    expect(screen.getByRole('img')).toBeInTheDocument()
+  })
+})
--- QR-Code/vite.config.ts
+++ QR-Code/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/QR-Code/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setupTests.ts',
+  }
 })
